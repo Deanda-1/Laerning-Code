8b42e8e09fa477e962bf6e3e7df72dfa1e9cfa5e
Your Name <you@example.com>
2024-04-09 22:27:33 -0400

    working

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-console.log("Hello Mom");
-let message = "working on it";
-let price = 20;
-console.log(message);
-console.log(price);
-let x = 30;
-console.log(x);
-x = 30 + 10;
-console.log(x);
-const speed = 70;
-console.log(speed);
-let X = 2;
-let Y = 2;
-let txt1 = "raw";
-let txt2 = " mangoes";
-console.log(txt1 + txt2);
+// console.log("Hello Mom");
+// let message = "working on it";
+// let price = 20;
+// console.log(message);
+// console.log(price);
+// let x = 30;
+// console.log(x);
+// x = 30 + 10;
+// console.log(x);
+// const speed = 70;
+// console.log(speed);
+// // let X = 2;
+// // let Y = 2;
+// let txt1 = "raw";
+// let txt2 = " mangoes";
+// console.log(txt1 + txt2);
+// let X = 5;
+// let Y = 5.123;
+// console.log(X + Y);
+// console.log(1/0);
+// console.log("This is a sentence"/5);
+// let str1 = "This is a string";
+// let str2 = 'It can also use single quotes';
+// let str3 = `${str1}, ${str2} Backticks can be used`;
+// console.log(str3);
+// bool1 = false;
+// bool2 = 5 > 3;
+// console.log(bool1, bool2);
+// let age = null;
+// console.log(age);
+// let Z;
+// console.log(Z);
+let person = {
+    firstName: "Daenda",
+    lastName: "Noble",
+    age: 55,
+    eyeColor: "black"
+};
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.age);
+// console.log(typeof(person));
+let X = 5,
+str = "This is a string.",
+bool1 = true,
+var1 = null,
+var2;
+console.log(typeOf(X));
+console.log(typeOf(str));
+console.log(typeOf(bool));
+console.log(typeOf(var1));
+console.log(typeOf(var2));
