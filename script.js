74f6c423e19654efc40643567c892775fa7532f7
Your Name <you@example.com>
2024-04-11 23:27:55 -0400

    still working on it

---
+++ script.js
@@ -0,0 +1,21 @@
+// function myfunction(p1, p2) {
+//     number = 5;
+//   return p1 * p2;
+// }
+
+// // x = myfunction(4, 3);
+// // console.log(x);
+
+// // y = myfunction(5, 4);
+// // z = myfunction(6, 8);
+// // console.log(y, z);
+
+// // console.log(myfunction(9, 4));
+// // console.log(myfunction);
+// console.log(number);
+myfunction = (p1, p2) => p1 * p2;
+
+console.log(myfunction(4, 5));
+
+myfunction = name => "My name is " + name;
+console.log(myfunction("Deanda"));
