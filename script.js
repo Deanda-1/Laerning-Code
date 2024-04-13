866eaf3bea18f87d17933d145041532767ec7d82
Your Name <you@example.com>
2024-04-13 13:16:34 -0400

    working

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-let person = {
-    firstName: "Deanda",
-    lastName: "Noble",
-    age: 55,
-  fullName() {
-    return "Hello " + this.firstName + " "
-    + this.lastName + " !";
- }
-};
-console.log(person);
-console.log(person.firstName);
-console.log(person["lastName"]);
-console.log(person.fullName());
-delete person.fullName;
-console.log(person);
+// let i = 0;
+// for (;;) {
+//     if(i<10) {
+//     console.log(i);
+//     }
+//     else break;
+//     i+=2;
+// }
+
+// let person = {
+//     firstName: "Deanda",
+//     lastName: "Noble",
+//     age: 55,
+//     lable: "Bride of Christ"
+// };
+// let x;
+// for(x in person) {
+//     console.log(x + " : " + person[x]);
+// }
+
+// let family = ['Grandfather', 'Dad', 'Mom', 'Sister', 'Brother', 'Little Brother'];
+// let y;
+// for(y of family) {
+//     console.log(y);
+// }
+
+// let text = "Javascript";
+
+// for(y of text) {
+//     console.log(y);
+// }
+
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while(i<10);
+
+// console.log("using while: ");
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+// }
+
+// console.log("using do while");
+// do {
+//     console.log(i);
+// } while(i < 10);
+
+// let family = ['Grandfather', 'Dad', 'Mom', 'Sister', 'Brother', 'Little Brother'];
+// console.log("using for loop:");
+// for(i = 0; i < family.length; i++) {
+//     console.log(family[i]);
+// }
+
+
+// console.log("using while loop:");
+// i = 0;
+// while(i < family.length) {
+//     console.log(family[i]);
+//     i++;
+// }
